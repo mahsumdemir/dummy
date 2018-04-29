d425e2e328f8b2268641fdfb1c1f1c9e9244fee9
Mahsum Demir <mahsumdemir93mdmd@gmail.com>
2018-04-29 20:45:08 +0300

    Add Param annotation

--- src/main/java/com/example/demo/PersonRepository.java
+++ src/main/java/com/example/demo/PersonRepository.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    List<NameProjection>  findByNameContains(String name);
+    List<NameProjection>  findByNameContains(@Param("name") String name);
 }
